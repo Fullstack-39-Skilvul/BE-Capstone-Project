308f9d92c3130e7637da5541d82f51daa8911660
vikiade00 <vikiade00@gmail.com>
2023-11-28 18:48:38 +0700

    Fix cors

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const allRoute = require("./route");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(allRoute);
 
 db.then(() => {
