9aad3d34bc6802ee45f40d7cb656d90e8025cd9a
vikiade00 <vikiade00@gmail.com>
2023-11-29 06:17:38 +0700

    fix token jenis konseling

--- route/jenisKonseling.route.js
+++ route/jenisKonseling.route.js
@@ -9,8 +9,8 @@ const {
 const verifyToken = require("../middleware/verifyToken.middleware");
 const route = express.Router();
 
-route.get("/", verifyToken(["admin"]), getAllJenisKonseling);
-route.get("/:id", verifyToken(["admin"]), getJenisKonselingById);
+route.get("/", getAllJenisKonseling);
+route.get("/:id", getJenisKonselingById);
 route.post("/", verifyToken(["admin"]), createJenisKonseling);
 route.put("/:id", verifyToken(["admin"]), editJenisKonseling);
 route.delete("/:id", verifyToken(["admin"]), deleteJenisKonseling);
