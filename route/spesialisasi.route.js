f066c2c404689db1240a142b9466d8204ed2a23b
vikiade00 <vikiade00@gmail.com>
2023-12-04 21:59:44 +0700

    Fix auth

--- route/spesialisasi.route.js
+++ route/spesialisasi.route.js
@@ -12,7 +12,7 @@ const route = express.Router();
 route.get("/", verifyToken(["admin", "konselor"]), getAllSpesialisasi);
 route.get("/:id", verifyToken(["admin", "konselor"]), getSpesialisasiById);
 route.post("/", verifyToken(["admin"]), createSpesialisasi);
-route.put("/:id", verifyToken(["admin"]), editSpesialisasi);
+route.put("/:id", verifyToken(["admin", "konselor"]), editSpesialisasi);
 route.delete("/:id", verifyToken(["admin"]), deleteSpesialisasi);
 
 module.exports = route;
